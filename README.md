ce6d0b8a46c875438bd988841d50038c8ecf1011
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2022-10-24 01:05:12 +0700

    edit README.md

--- README.md
+++ README.md
@@ -42,7 +42,65 @@ _Ở project này, mình minh hoạ cách chọn phần tử chốt ở cuối d
    - PURPLE: màu phần tử lớn hơn phần tử pivot.
    - ORANGE: màu phần tử đã được sắp xếp đúng vị trí.
    Code:
-    
+     // arr[] --> Mảng cần được sắp xếp,
+     // low --> chỉ mục bắt đầu,
+     // high --> chỉ mục kết thúc
+     private void quickSort(CNode[] arr, int low, int high) {
+     if (low < high) {
+     // q là chỉ mục của chốt, arr[q] là vị trí của chốt
+     int q = partition(arr, low, high);
+     // Sắp xếp đệ quy các phần tử
+     // trước phân vùng và sau phân vùng
+     quickSort(arr, low, q - 1);
+     quickSort(arr, q + 1, high);
+     } else if (low == high) {
+     // xử lý trường hợp lỗi 1 phần tử ko đổi màu
+     arr[high].setRightColor(true);
+     transitions.add(colorCNode(arr, SORTED_COLOR, true, high));
+
+     }
+     }
+
+    // Hàm nhận phần tử cuối cùng làm chốt,
+    // đặt các phần tử nhỏ hơn chốt ở trước
+    // và lớn hơn ở sau nó
+    private int partition(CNode[] arr, int low, int high) {
+    // pivot (Element to be placed at right position)
+    CNode pivot = arr[high];
+    // đổi màu của pivot sang PIVOT_COLOR (YELLOW)
+    transitions.add(colorCNode(arr, PIVOT_COLOR, true, high));
+    int i = low - 1; // vị trí của phần tử nhỏ hơn
+
+        for (int j = low; j < high; j++) {
+            // đổi màu của phần tử arr[j] sang SELECT_COLOR (RED)
+            transitions.add(colorCNode(arr, SELECT_COLOR, true, j));
+            // nếu phần tử hiện tại nhỏ hơn chốt
+            if (arr[j].getValue() <= pivot.getValue()) {
+                transitions.add(colorCNode(arr, SMALLER_COLOR, true, j)); // đổi màu sang SMALLER_COLOR (GREEN)
+                i++;
+                transitions.add(swap(arr, i, j));
+            } else {
+                transitions.add(colorCNode(arr, BIGGER_COLOR, true, j)); // đổi màu sang BIGGER_COLOR(PURPLE)
+            }
+        }
+
+        transitions.add(swap(arr, i + 1, high));
+
+
+        int sorted = i + 1;
+        arr[sorted].setRightColor(true);
+        transitions.add(colorCNode(arr, SORTED_COLOR, true, sorted));
+
+        for (int j = 0; j < arr.length; j++) {
+            if (j != sorted) {
+                if (!arr[j].isRightColor()) {
+                    transitions.add(colorCNode(arr, START_COLOR, false, j));
+                }
+            }
+        }
+
+        return i + 1;
+    }
    
 ### 4. Độ phức tạp của giải thuật
 
--- src/main/java/org/example/sortingalgorithm/QuickSort.java
+++ src/main/java/org/example/sortingalgorithm/QuickSort.java
@@ -1,92 +1,90 @@
 package org.example.sortingalgorithm;
 
-import org.example.cnode.CNode;
 import javafx.animation.Transition;
 import javafx.scene.paint.Color;
+import org.example.cnode.CNode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class QuickSort extends AbstractSort {
-	private static final Color PIVOT_COLOR = Color.YELLOW;
-	private static final Color BIGGER_COLOR = Color.PURPLE;
-	private static final Color SMALLER_COLOR = Color.GREEN;
-	private static final Color SELECT_COLOR = Color.RED;
-	private ArrayList<Transition> transitions;
+    private static final Color PIVOT_COLOR = Color.YELLOW;
+    private static final Color BIGGER_COLOR = Color.PURPLE;
+    private static final Color SMALLER_COLOR = Color.GREEN;
+    private static final Color SELECT_COLOR = Color.RED;
+    private final ArrayList<Transition> transitions;
 
-	public QuickSort() {
-		this.transitions = new ArrayList<>();
-	}
+    public QuickSort() {
+        this.transitions = new ArrayList<>();
+    }
 
-	private void quickSort(CNode[] arr, int low, int high) {
-		if (low < high) {
-			int q = partition(arr, low, high);
-			quickSort(arr, low, q - 1);
-			quickSort(arr, q + 1, high);
-		} else if (low == high) {
-			// xử lý trường hợp lỗi 1 phần tử ko đổi màu
-			arr[high].setRightColor(true);
-			transitions.add(colorCNode(arr, SORTED_COLOR, true, high));
+    // arr[] --> Mảng cần được sắp xếp,
+    // low --> chỉ mục bắt đầu,
+    // high --> chỉ mục kết thúc
+    private void quickSort(CNode[] arr, int low, int high) {
+        if (low < high) {
+            // q là chỉ mục của chốt, arr[q] là vị trí của chốt
+            int q = partition(arr, low, high);
+            // Sắp xếp đệ quy các phần tử
+            // trước phân vùng và sau phân vùng
+            quickSort(arr, low, q - 1);
+            quickSort(arr, q + 1, high);
+        } else if (low == high) {
+            // xử lý trường hợp lỗi 1 phần tử ko đổi màu
+            arr[high].setRightColor(true);
+            transitions.add(colorCNode(arr, SORTED_COLOR, true, high));
 
-		}
-	}
+        }
+    }
 
-	/*
-	 * private void checkSort(CNode[] arr, int i,boolean c){ if(c == true){
-	 * transitions.add(colorCNode(arr,SORTED_COLOR,i)); } else
-	 * transitions.add(colorCNode(arr,START_COLOR,i)); }
-	 */
-	private int partition(CNode[] arr, int low, int high) {
-		// Boolean[] array = new Boolean[arr.length-1];
-		// Arrays.fill(array, Boolean.FALSE);
-		CNode pivot = arr[high];
-		// pivot (Element to be placed at right position)
-		transitions.add(colorCNode(arr, PIVOT_COLOR, true, high));
+    // Hàm nhận phần tử cuối cùng làm chốt,
+    // đặt các phần tử nhỏ hơn chốt ở trước
+    // và lớn hơn ở sau nó
+    private int partition(CNode[] arr, int low, int high) {
+        // pivot (Element to be placed at right position)
+        CNode pivot = arr[high];
+        // đổi màu của pivot sang PIVOT_COLOR (YELLOW)
+        transitions.add(colorCNode(arr, PIVOT_COLOR, true, high));
+        int i = low - 1; // vị trí của phần tử nhỏ hơn
 
-		int i = low - 1;
+        for (int j = low; j < high; j++) {
+            // đổi màu của phần tử arr[j] sang SELECT_COLOR (RED)
+            transitions.add(colorCNode(arr, SELECT_COLOR, true, j));
+            // nếu phần tử hiện tại nhỏ hơn chốt
+            if (arr[j].getValue() <= pivot.getValue()) {
+                transitions.add(colorCNode(arr, SMALLER_COLOR, true, j)); // đổi màu sang SMALLER_COLOR (GREEN)
+                i++;
+                transitions.add(swap(arr, i, j));
+            } else {
+                transitions.add(colorCNode(arr, BIGGER_COLOR, true, j)); // đổi màu sang BIGGER_COLOR(PURPLE)
+            }
+        }
 
-		for (int j = low; j < high; j++) {
-			transitions.add(colorCNode(arr, SELECT_COLOR, true, j));
+        transitions.add(swap(arr, i + 1, high));
 
-			if (arr[j].getValue() <= pivot.getValue()) {
-				transitions.add(colorCNode(arr, SMALLER_COLOR, true, j));
-				i++;
-				transitions.add(swap(arr, i, j));
-			} else {
-				transitions.add(colorCNode(arr, BIGGER_COLOR, true, j));
-			}
-		}
 
-		transitions.add(swap(arr, i + 1, high));
+        int sorted = i + 1;
+        arr[sorted].setRightColor(true);
+        transitions.add(colorCNode(arr, SORTED_COLOR, true, sorted));
 
-		/*
-		 * int k=i+1; int l = arr.length-1; array[k] = true; while(l>0){
-		 * checkSort(arr,l,array[l]); l--;}
-		 */
+        for (int j = 0; j < arr.length; j++) {
+            if (j != sorted) {
+                if (!arr[j].isRightColor()) {
+                    transitions.add(colorCNode(arr, START_COLOR, false, j));
+                }
+            }
+        }
 
-		int sorted = i + 1;
-		arr[sorted].setRightColor(true);
-		transitions.add(colorCNode(arr, SORTED_COLOR, true, sorted));
+        return i + 1;
+    }
 
-		for (int j = 0; j < arr.length; j++) {
-			if (j != sorted) {
-				if (!arr[j].isRightColor()) {
-					transitions.add(colorCNode(arr, START_COLOR, false, j));
-				}
-			}
-		}
+    @Override
+    public ArrayList<Transition> startSort(CNode[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
 
-		// transitions.add(colorCNode(arr,PIVOT_COLOR,i+1));*/
-		return i + 1;
-	}
-
-	@Override
-	public ArrayList<Transition> startSort(CNode[] arr) {
-		quickSort(arr, 0, arr.length - 1);
-		transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
-
-		return transitions;
-	}
+        return transitions;
+    }
 
 }
 
